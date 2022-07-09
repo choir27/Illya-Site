47d148874e3d1dd151e4e9060bfd743811fb7325
choir27 <richardchoi54@gmail.com>
2022-07-08 21:13:03 -0400

    add node/express/ejs for backend and mongodb for database

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 9000
+const cors = require('cors')
+const fs = require('fs')
+require('dotenv').config()
+
+let db = 'illyaComments'
+let dbName = 'illyaComments'
+
+MongoClient.connect(process.env.DATABASE_URL, { useUnifiedTopology: true })
+    .then(client => {
+
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+    
+app.set('view engine', 'ejs')
+app.use(express.static('views'))
+app.use(express.static('js'))
+app.use(express.static('css'))
+app.use(express.static('assets'))
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+
+app.get('/',(request, response)=>{
+    db.collection('illyaComments').find().toArray()
+        .then(result => {
+            response.render('home.ejs', { info: result })
+        })
+        .catch(err=>console.error(err))
+})
+
+app.get('/gameplay',(request, response)=>{
+    db.collection('illyaComments').find().toArray()
+        .then(result => {
+            response.render('gameplay.ejs', { info: result })
+        })
+        .catch(err=>console.error(err))
+})
+
+app.get('/about',(request, response)=>{
+    db.collection('illyaComments').find().toArray()
+        .then(result => {
+            response.render('about.ejs', { info: result })
+        })
+        .catch(err=>console.error(err))
+})
+
+app.get('/photos',(request, response)=>{
+    db.collection('illyaComments').find().toArray()
+        .then(result => {
+            response.render('photos.ejs', { info: result })
+        })
+        .catch(err=>console.error(err))
+})
+
+app.listen(process.env.PORT || PORT, ()=>{  
+    console.log(`Server running on port ${PORT}`)
+})
